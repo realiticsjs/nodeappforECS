d1bcf2815848e1848d3883874752aeed5f71a48b
anuragtechhub <102394648+anuragtechhub@users.noreply.github.com>
2022-05-21 10:49:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Java Home App - version-10!!</h1> \n');
+  res.send('<h2 style="color:red;">WELCOME TO SOLASALONSTUDIOS</h2> \n');
 });
 
 app.listen(PORT, HOST);
