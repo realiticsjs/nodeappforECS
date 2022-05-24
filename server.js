5d0b7cc9911bac1beddcac2804123276678fc8c6
Kumar Gaurav <103304785+realiticsjs@users.noreply.github.com>
2022-05-24 14:45:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h2 style="color:pink;">WELCOME TO solaconnect-backend-dev</h2> \n');
+  res.send('<h2 style="color:green;">WELCOME TO solaconnect-backend-dev</h2> \n');
 });
 
 app.listen(PORT, HOST);
